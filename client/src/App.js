4f0cac6f5d254360d981a97a2f34ec93aa4010f5
Nguyễn Đăng Thắng <56484113+unikey1919@users.noreply.github.com>
2021-07-01 22:36:46 +0700

    redux workflow + UI set

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import React, { useEffect } from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
@@ -8,16 +10,20 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    return(
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+    return (
         <Container maxWidth="lg">
-            <AppBar position = "static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="400"/>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify ="space-between" alignItems="stretch" spacing = {3}>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item x2={12} sm={7}>
                             <Posts />
                         </Grid>
