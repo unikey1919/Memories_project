54e59f101eee429e3b9ec534f1c4813703263e58
Nguyễn Đăng Thắng <56484113+unikey1919@users.noreply.github.com>
2021-07-03 12:24:40 +0700

    UI + add data + retrieve

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getPosts, createPosts} from '../controllers/posts.js'
+import { getPosts, createPost} from '../controllers/posts.js'
 
 const router = express.Router();
 
 // http://localhost:5000/posts
 router.get('/', getPosts);
-router.get('/', createPosts);
+router.post('/', createPost);
 
 export default router;
