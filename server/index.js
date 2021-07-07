2731b1d78fcaaa35b9b774adb37f84066809e0f3
Nguyễn Đăng Thắng <56484113+unikey1919@users.noreply.github.com>
2021-07-07 14:52:37 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello 47');
+});
 
 //const CONNECTION_URL = 'mongodb+srv://47:47@cluster0.m1c8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
